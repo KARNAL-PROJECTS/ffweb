ec0bcf70b98233b2169e3c293769ac1a9db6ebfa
KARNAL-PROJECTS <seraza934@gmail.com>
2026-03-17 14:28:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ HTML_TEMPLATE = '''<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>STILLRARE | Master Hub</title>
+    <title>BAKI FF TOOL | Master Hub</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
